ce244407d981b44fe26d99d4babda82f3520d480
Raj Kumar Bhol <137034819+RajKumarBhol@users.noreply.github.com>
2025-07-11 11:49:24 +0530

    Update currency_convertor.js

--- Currency_Convertor/currency_convertor.js
+++ Currency_Convertor/currency_convertor.js
@@ -99,5 +99,6 @@ async function convert() {
   } catch (error) {
     console.error("Conversion error:", error);
     resultElement.textContent = "Conversion failed. Please try again.";
+    
   }
 }
